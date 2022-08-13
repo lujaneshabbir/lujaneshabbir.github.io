553fc96691978d94dec70e7b32351685004e4a4c
lujaneshabbir <60037203+lujaneshabbir@users.noreply.github.com>
2022-08-13 22:59:17 +1200

    added jquery to show contact form + some other jquery

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 $(document).ready(() => {
-  $('#contact-button').click(function(){
-    alert("CLICK");
+  $('#contact-icon').click(function(){
+    $("#contact-window").show(1000);
+  })
+
+  $('.paint-tools img').click(function(){
+    $('.paint-tools img').removeClass('border-3din');
+    $('.paint-tools img').addClass('border-3d');
+    $(this).removeClass("border-3d");
+    $(this).addClass("border-3din");
   })
 })
