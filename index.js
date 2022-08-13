880a5f42b4cee573650f428112fdd8b8797348e0
lujaneshabbir <60037203+lujaneshabbir@users.noreply.github.com>
2022-08-13 23:17:47 +1200

    close window functionality added

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ $(document).ready(() => {
   $('#contact-icon').click(function(){
     $("#contact-window").show(1000);
   })
-
   $('.paint-tools img').click(function(){
     $('.paint-tools img').removeClass('border-3din');
     $('.paint-tools img').addClass('border-3d');
     $(this).removeClass("border-3d");
     $(this).addClass("border-3din");
+  })
+
+  $('.x-button').click(function(){
+    // add delay to show x button clicked
+    // $(this).removeClass("border-3d");
+    // $(this).addClass("border-3din");
+    $(this).parent().parent().parent().hide(1000);
   })
 })
